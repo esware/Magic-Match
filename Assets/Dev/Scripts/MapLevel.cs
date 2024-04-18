13b705e8b80e2555e47c7b4e222f15247c33afca
Serhat <48649947+esware@users.noreply.github.com>
2024-04-18 14:54:25 +0300

    created map Level scripts , for all levels
    Former-commit-id: 26fe8d0b92abd8b95f87831cf51ee64f8a021017

---
+++ Assets/Dev/Scripts/MapLevel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Vector2 = System.Numerics.Vector2;
+
+namespace Dev.Scripts
+{
+    public class MapLevel:MonoBehaviour
+    {
+        private Vector3 _originalScale;
+        private bool _isScaled;
+        
+        public float overScale = 1.05f;
+        public float clickScale = 0.95f;
+        public int number;
+        public bool isLocked;
+        public Transform @lock;
+        public Transform pathPivot;
+        public Object levelScene;
+        public string sceneName;
+        public int starsCount;
+        public Transform starsHoster;
+        public Transform star1;
+        public Transform star2;
+        public Transform star3;
+
+        private void Awake()
+        {
+            _originalScale = transform.localScale;
+        }
+
+        #region Enable click
+        
+        #endregion
+        
+        public void UpdateState(int starsCount,bool isLocked)
+        {
+            this.starsCount = starsCount;
+            this.isLocked = isLocked;
+            UpdateStars(starsCount);
+            @lock.gameObject.SetActive(isLocked);
+        }
+        public void UpdateStars(int starsCount)
+        {
+            star1.gameObject.SetActive(starsCount >= 1);
+            star2.gameObject.SetActive(starsCount >= 2);
+            star3.gameObject.SetActive(starsCount >= 3);
+        }
+    }
+}
