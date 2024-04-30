99d50d19ffcc4c160b445f771b5a974733d5ea0b
Serhat <48649947+esware@users.noreply.github.com>
2024-05-01 00:46:33 +0300

    Admob
    Former-commit-id: 0e36cfb1ca90294df1370532f4c5ccddeb1c7056

---
+++ Assets/Dev/Scripts/GoogleRewardedAds/RewAdmobManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+namespace Dev.Scripts.GoogleRewardedAds
+{
+    public class RewAdmobManager:MonoBehaviour
+    {
+        public static RewAdmobManager Instance;
+        private RewardedAd _rewardVideoAd;
+        private Action _resultCallBack;
+
+        private void Awake()
+        {
+            if (Instance==null)
+                Instance = this;
+            else if (Instance!=this)
+                Destroy(gameObject);
+            DontDestroyOnLoad(this);
+        }
+
+        private void Start()
+        {
+           
+        }
+    }
+}
